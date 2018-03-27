a510bcc0ad6cefb8f0c12904fa739e95880b705b
Michel Atieno <mishqamish@gmail.com>
2018-03-27 11:51:01 +0300

    hid side-bar

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,7 @@ function closeSlideMenu(){
   document.getElementById('side-menu').style.width='0';
   document.getElementById('main').style.marginLeft='0';
 }
+
+$(".navbar").click(function(){
+    $(".side-nav").toggle();
+});
